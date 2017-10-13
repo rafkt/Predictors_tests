93d406c3bf061e60a2078b2d5f9af25b322e8b40
Rafael Ktistakis <rafaelktistakis@gmail.com>
2017-10-13 15:43:33 +0100

    Slightly changed the script

--- initExperiments_with_all_possible_parameters.sh
+++ initExperiments_with_all_possible_parameters.sh
@@ -5,7 +5,17 @@
 
 clear
 
-declare -a arr=("BIBLE.txt" "BMS1_spmf.txt" "BMS2.txt" "FIFA.txt" "Kosarak_converted.txt" "LEVIATHAN.txt" "MSNBC.txt" "NASA_access_log_Aug95.txt" "NASA_access_log_Jul95.txt" "SIGN.txt")
+declare -a arr=("BIBLE_CHARProfile.java"
+                "BMSProfile.java"    
+                "FIFAProfile.java"   
+                "MSNBCProfile.java" 
+                "ProfileManager.java"
+                "SNAKEProfile.java"
+                "BIBLE_WORDProfile.java"
+                "DefaultProfile.java"
+                "KOSARAKProfile.java"
+                "SIGNProfile.java"    
+                "SPICEProfile.java")
 
 
 
@@ -13,49 +23,51 @@ declare -a arr=("BIBLE.txt" "BMS1_spmf.txt" "BMS2.txt" "FIFA.txt" "Kosarak_conve
 for i in `seq -f "%g" 0.1 0.1 1`
 do 
     for j in `seq -f "%g" 1 1 9`
-    do       
+    do
+        cd src/ca/ipredict/predictor/profile/   
         for d in "${arr[@]}"
         do
-            sed "s/parameters.put(\"noiseRatio\", \"1.0f\");/parameters.put(\"noiseRatio\", \"1.2f\");/g" SPICEProfile.java > tmp.java
-            mv tmp.java 
+            sed "s/parameters.put(\"noiseRatio\", \"1.0f\");/parameters.put(\"noiseRatio\", \"1.2f\");/g" $d > "$d_tmp$i$j".java
+            # mv tmp.java 
         done
+        cd ../../../../..
     done
 done
 
 
-declare -a arr=("100"
-				"200"
-				"300"
-				"400")
-# 				"500"
-# 				"600"
-# 				"700"
-# 				"800"
-# 				"900")
-				# "1000"
-				# "1500"
-				# "2000")
+# declare -a arr=("100"
+# 				"200"
+# 				"300"
+# 				"400")
+# # 				"500"
+# # 				"600"
+# # 				"700"
+# # 				"800"
+# # 				"900")
+# 				# "1000"
+# 				# "1500"
+# 				# "2000")
 
 
-for i in "${arr[@]}"
-do
-        echo $i
+# for i in "${arr[@]}"
+# do
+#         echo $i
 
-        if [ "$i" -ne "100" ]; then
-                sed "s/evaluator.addDataset(Format.QUEST$previous,-1);/evaluator.addDataset(Format.QUEST$i,-1);/g" src/ca/ipredict/controllers/MainController.java > tmp.java
-                mv tmp.java src/ca/ipredict/controllers/MainController.java
+#         if [ "$i" -ne "100" ]; then
+#                 sed "s/evaluator.addDataset(Format.QUEST$previous,-1);/evaluator.addDataset(Format.QUEST$i,-1);/g" src/ca/ipredict/controllers/MainController.java > tmp.java
+#                 mv tmp.java src/ca/ipredict/controllers/MainController.java
 
-        fi
+#         fi
 
-        ant build
-        cd bin
-        java -Xmx12g -Xms12g -XX:-UseConcMarkSweepGC ca.ipredict.controllers.MainController
+#         ant build
+#         cd bin
+#         java -Xmx12g -Xms12g -XX:-UseConcMarkSweepGC ca.ipredict.controllers.MainController
 
-        #mv testing_seq $(echo $i"_testing_seq")
-        #mv training_seq $(echo $i"_training_seq")
+#         #mv testing_seq $(echo $i"_testing_seq")
+#         #mv training_seq $(echo $i"_training_seq")
 
-        cd ..
+#         cd ..
 
-	previous=$i
+# 	previous=$i
 
-done
+# done
