83578d700b810d14ff9640f246b5b7b974e7c599
Rafael Ktistakis <rafaelktistakis@gmail.com>
2019-03-11 16:43:47 +0000

    minor correction

--- src/ca/ipredict/predictor/Evaluator.java
+++ src/ca/ipredict/predictor/Evaluator.java
@@ -317,7 +317,7 @@ public class Evaluator {
 		FileWriter fileWriter = null, fileWriter2 = null;
 		BufferedWriter bufferedWriter = null, bufferedWriter2 = null;
 
-		String fileName = "outputs/" + datasetName + ".sigma.seq." + trainingSequences.size() + ".training.txt";
+		String fileName = "outputs/" + datasetName + ".length.seq." + trainingSequences.size() + ".training.txt";
 		try{
 	        fileWriter =
 	            new FileWriter(fileName);
@@ -363,10 +363,10 @@ public class Evaluator {
 		try {bufferedWriter.close();} catch (IOException ex){}
 
 
-		fileName = "outputs/" + datasetName + ".sigma.seq." + trainingSequences.size() + ".consequent.txt";
+		fileName = "outputs/" + datasetName + ".length.seq." + trainingSequences.size() + ".consequent.txt";
 
 
-        String fileName2 = "outputs/" + datasetName + ".sigma.seq." + trainingSequences.size() + ".queries.txt";
+        String fileName2 = "outputs/" + datasetName + ".length.seq." + trainingSequences.size() + ".queries.txt";
 
         try{
 
