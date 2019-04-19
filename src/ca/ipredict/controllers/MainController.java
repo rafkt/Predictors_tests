b3518e42753a2e1ab2bc632b49f895e5754f3fcf
Rafael Ktistakis <rafaelktistakis@gmail.com>
2019-04-19 11:32:54 +0100

    still exporting

--- src/ca/ipredict/controllers/MainController.java
+++ src/ca/ipredict/controllers/MainController.java
@@ -63,12 +63,12 @@ public class MainController {
 			Evaluator evaluator = new Evaluator(args[0]);
 			
 			//Loading datasets
-			evaluator.addDataset("BMS", 		4830);
-			evaluator.addDataset("SIGN", 		1000);
+			//evaluator.addDataset("BMS", 		4830);
+			//evaluator.addDataset("SIGN", 		1000);
 				// evaluator.addDataset("MSNBC", 		5000);
 				// evaluator.addDataset("BIBLE_WORD", 	5000);
 				// evaluator.addDataset("BIBLE_CHAR", 	5000);
-		//evaluator.addDataset("KOSARAK", 	45000);
+		evaluator.addDataset("KOSARAK", 	45000);
 			//evaluator.addDataset("FIFA", 		5000);
 
 			// evaluator.addDataset("SPICE0", 		5000);
@@ -93,7 +93,7 @@ public class MainController {
 			// evaluator.addPredictor(new TDAGPredictor());
 			// evaluator.addPredictor(new CPTPlusPredictor("CPT+",		"CCF:true CBS:true"));
 			// evaluator.addPredictor(new CPTPredictor());
-			evaluator.addPredictor(new CPT_Approx("CPT_App_" + 10, 10));
+			evaluator.addPredictor(new CPT_Approx("CPT_App_" + 11, 11));
 			// evaluator.addPredictor(new MarkovFirstOrderPredictor());
 			// evaluator.addPredictor(new MarkovAllKPredictor());
 			// evaluator.addPredictor(new LZ78Predictor());
