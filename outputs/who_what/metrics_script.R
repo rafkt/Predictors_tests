a617fb2f3befd410dedf6d75119e047442cc59fa
Rafael Ktistakis <rafaelktistakis@gmail.com>
2019-05-28 19:17:18 +0100

    debugged an error affecting answers on logic expressions; OR and AND operations should be done with | and & respectively

--- outputs/who_what/metrics_script.R
+++ outputs/who_what/metrics_script.R
@@ -109,3 +109,98 @@ average_pearson_cor = function(dataset, predictorA, predictorB){
   }
   return(c(t = sum_t/14, df = sum_df/14, p_value = sum_pvalue/14))
 }
+
+# The following functions, calculate usefull queries. Such queries can be in the form; "Somebody predicted correctly while somebody else not"..
+# Each predictor's index in the data can be found with e.g. bible_char[[1]][1:8] which will return 
+# [1] "DG"        "TDAG"      "CPT+"      "CPT"       "Mark1"     "AKOM"      "LZ78"      "CPT_App_0"
+# This order should be the same for every dataset as we have it, since data was auto-generated through code experiments.
+# Feel free to expand queries as needed later; all the functionality is included below. Also look at notion "usefull commands" for further commands
+# that can be useful to me.. 
+lossless_yes_lossy_no = function(dataset){
+  av_per = 0
+  #for (fold in 1:14){
+  fold = 4
+    sum = 0
+    result <-           as.numeric(dataset[[fold]][3,2:ncol(dataset[[fold]])])
+    result <- result | as.numeric(dataset[[fold]][4,2:ncol(dataset[[fold]])])
+    result <- result | as.numeric(dataset[[fold]][8,2:ncol(dataset[[fold]])])
+    
+    result <- result & !as.numeric(dataset[[fold]][1,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][2,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][5,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][6,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][7,2:ncol(dataset[[fold]])])
+    
+    sum = sum(TRUE == result[1:length(result)])
+    print(sum)
+    
+    av_per = av_per + sum/length(result)
+  #}
+  
+    return(result) 
+  #return(c(Percentage = av_per/1 * 100))
+}
+
+lossy_yes_lossless_no = function(dataset){
+  av_per = 0
+  #for (fold in 1:14){
+  fold = 4
+    sum = 0
+    result <-          as.numeric(dataset[[fold]][1,2:ncol(dataset[[fold]])])
+    result <- result || as.numeric(dataset[[fold]][2,2:ncol(dataset[[fold]])])
+    result <- result || as.numeric(dataset[[fold]][5,2:ncol(dataset[[fold]])])
+    result <- result || as.numeric(dataset[[fold]][6,2:ncol(dataset[[fold]])])
+    result <- result || as.numeric(dataset[[fold]][7,2:ncol(dataset[[fold]])])
+    
+    result <- result & !as.numeric(dataset[[fold]][3,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][4,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][8,2:ncol(dataset[[fold]])])
+    
+    sum = sum(TRUE == result[1:length(result)])
+    print(sum) 
+    av_per = av_per + sum/length(result)
+  #}
+  
+  return(c(Percentage = av_per/1 * 100))
+}
+
+cptPlus_yes_subSeq_no = function(dataset){
+  av_per = 0
+  for (fold in 1:14){
+    sum = 0
+    result <-          as.numeric(dataset[[fold]][3,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][8,2:ncol(dataset[[fold]])])
+    
+    sum = sum(TRUE == result[1:length(result)])
+    
+    av_per = av_per + sum/length(result)
+  }
+  
+  return(c(Percentage = av_per/14 * 100))
+}
+
+subseq_yes_cptPlus_no = function(dataset){
+  av_per = 0
+  for (fold in 1:14){
+    sum = 0
+    result <-          as.numeric(dataset[[fold]][8,2:ncol(dataset[[fold]])])
+    result <- result & !as.numeric(dataset[[fold]][3,2:ncol(dataset[[fold]])])
+    
+    sum = sum(TRUE == result[1:length(result)])
+    
+    av_per = av_per + sum/length(result)
+  }
+  
+  return(c(Percentage = av_per/14 * 100))
+}
+
+load_all_datasets = function(){
+  Bms <<- prepare("BMS.*.csv", "sBP.BMS.*.csv")
+  Sign <<- prepare("SIGN.*.csv", "sBP.SIGN.*.csv")
+  Msnbc <<- prepare("MSNBC.*.csv", "sBP.MSNBC.*.csv")
+  Bible_w <<- prepare("BIBLE_WORD.*.csv", "sBP.BIBLE_WORD.*.csv")
+  Bible_c <<- prepare("BIBLE_CHAR.*.csv", "sBP.BIBLE_CHAR.*.csv")
+  Kosarak <<- prepare("KOSARAK.*.csv", "sBP.KOSARAK.*.csv")
+  Fifa <<- prepare("FIFA.*.csv", "sBP.FIFA.*.csv")
+}
+
